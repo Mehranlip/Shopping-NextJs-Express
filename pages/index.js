414e464a126acd36ad3b566f4884ad1d1d244761
Mehranlip <realmehranasadi@gmail.com>
2023-04-06 14:31:27 +0430

    add cart product and data file

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Layout from "../components/Layout"
+import Product from "../components/Product"
+import productItems from "../data/products.json"
 
 
 function Home() {
   return (
     <Layout title='Home Page'>
-
+      <div className="grid gride-cols-1 gap-12 md:grid-cols-3 lg:grid-cols-4">
+        {productItems.map((pItem) => (
+          <Product item={pItem} key={pItem.slug}></Product>
+        ))}
+      </div>
     </Layout>
   )
 }
