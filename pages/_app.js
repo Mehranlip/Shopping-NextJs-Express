4b91faeb97262007deb36e48ef477c8ad717939c
Mehranlip <realmehranasadi@gamil.com>
2023-05-09 15:05:23 +0430

    fix check user login

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,43 @@
 import '../styles/globals.css'
+
 import { CartContextProvider } from '../context/Cart'
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return <div className='bg-gray-100 '>
-    <SessionProvider session={pageProps.session}>
-      <CartContextProvider>
-        <Component {...pageProps} />
-      </CartContextProvider>
-    </SessionProvider>
-  </div>
+  return (
+    <div className='bg-gray-100'>
+      <SessionProvider session={session}>
+        <CartContextProvider>
+          {Component.auth ? (
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
+            <Component {...pageProps} />
+          )}
+        </CartContextProvider>
+      </SessionProvider>
+    </div>
+  )
+}
+
+function Auth({ children }) {
+  const router = useRouter()
+
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/unauthorized')
+    },
+  })
+
+  if (status === 'loading') {
+    return 'Loading'
+  }
+
+  return children
 }
 
 export default MyApp
--- pages/shipping.js
+++ pages/shipping.js
@@ -13,57 +13,73 @@ import Cookies from "js-cookie"
 
 
 function Shipping() {
-    const { state, dispatch } = useContext(CartContext)
-    const router = useRouter()
-    const { cart } = state
-    const { shippingData } = cart
     const { handleSubmit, setValue, register } = useForm()
 
+    const { state, dispatch } = useContext(CartContext)
 
+    const { cart } = state
+    const { shippingData } = cart
 
     useEffect(() => {
         setValue('name', shippingData.name)
         setValue('address', shippingData.address)
         setValue('postalCode', shippingData.postalCode)
-    }, [setValue, shippingData.name, shippingData.address, shippingData.postalCode])
+    }, [
+        setValue,
+        shippingData.name,
+        shippingData.address,
+        shippingData.postalCode,
+    ])
+
+    const router = useRouter()
+
     function submitHandler({ name, address, postalCode }) {
         dispatch({
-            type: "SAVE_SHIPPING-DATA",
-            payload: {
-                name, address, postalCode
-            },
+            type: 'SAVE_SHIPPING_DATA',
+            payload: { name, address, postalCode },
         })
-        Cookies.set('cart', JSON.stringify({
-            ...cart, shippingData: {
-                name, address, postalCode,
-            },
-        })
+
+        Cookies.set(
+            'cart',
+            JSON.stringify({
+                ...cart,
+                shippingData: {
+                    name,
+                    address,
+                    postalCode,
+                },
+            })
         )
-        router.push("/payment")
+
+        router.push('/payment')
     }
 
 
-    return <Layout title="Shipping">
-        <CheckoutWizard activeStep={1} />
-        <form className='mx-auto max-w-screen-md bg-white shadow-md rounded-xl px-8 pt-6 pb-8 mb-4' onSubmit={handleSubmit(submitHandler)}>
-            <h2 className='mb-4 text-xl font-semibold'>Shipping</h2>
-            <div className='mb-4'>
-                <input {...register('name')} className='shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ' id='name' placeholder='Name' autoFocus />
-            </div>
-            <div className='mb-4'>
-                <input {...register('address')} className='shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ' id='address' placeholder='Address' autoFocus />
-            </div>
-            <div className='mb-4'>
-                <input {...register("postalCode")} className='shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ' id='postalCode' placeholder='Postal Code' autoFocus />
-            </div>
-            <div className='mb-4'>
-                <button className='px-2 py-1 text-white  text-xl rounded-xl w-28 bg-gradient-to-r  from-pink-600 from-10% via-sky-500 via-30% to-emerald-500 to-90%'>
-                    Next
-                </button>
-            </div>
-        </form>
-    </Layout>
+    return (
+        <Layout title="Shipping">
+            <CheckoutWizard activeStep={1} />
+            <form className='mx-auto max-w-screen-md bg-white shadow-md rounded-xl px-8 pt-6 pb-8 mb-4' onSubmit={handleSubmit(submitHandler)}>
+                <h2 className='mb-4 text-xl font-semibold'>Shipping</h2>
+                <div className='mb-4'>
+                    <input {...register('name')} className='shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ' id='name' placeholder='Name' autoFocus />
+                </div>
+                <div className='mb-4'>
+                    <input {...register('address')} className='shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ' id='address' placeholder='Address' autoFocus />
+                </div>
+                <div className='mb-4'>
+                    <input {...register("postalCode")} className='shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ' id='postalCode' placeholder='Postal Code' autoFocus />
+                </div>
+                <div className='mb-4'>
+                    <button className='px-2 py-1 text-white  text-xl rounded-xl w-28 bg-gradient-to-r  from-pink-600 from-10% via-sky-500 via-30% to-emerald-500 to-90%'>
+                        Next
+                    </button>
+                </div>
+            </form>
+        </Layout>
+    )
 }
+
+Shipping.auth = true
 
 
 export default dynamic(() => Promise.resolve(Shipping), { ssr: false })
