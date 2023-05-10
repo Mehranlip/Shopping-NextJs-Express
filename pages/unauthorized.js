923e673baf3d1c0d37edb1c51033acf65e3313f3
Mehranlip <realmehranasadi@gamil.com>
2023-05-10 14:21:57 +0430

    fix auth

--- pages/unauthorized.js
+++ pages/unauthorized.js
@@ -1,10 +1,9 @@
-import Layout from "../components/Layout"
-
+import Layout from '../components/Layout'
 
 function Unauthorized() {
     return (
-        <Layout title="Access Denied">
-            <h2 className="mb-4 text-white px-2 py-1 text-xl rounded-xl bg-gradient-to-r  from-pink-600 from-10% via-sky-500 via-30% to-emerald-500 to-90%">Access Denied</h2>
+        <Layout title='Access Denied'>
+            <h2 className='text-xl'>Access Denied</h2>
         </Layout>
     )
 }
